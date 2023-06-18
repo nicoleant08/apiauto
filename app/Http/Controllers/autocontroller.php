7a393b650be89aa5b2f854ad8280d24033e7cea4
Nicole Garcia <nikkgarcia299@gmail.com>
2023-06-18 16:50:46 -0300

    no entiendo mucho pero hice controller,factory, rutas,modelo, y seeds

---
+++ app/Http/Controllers/autocontroller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\auto;
+
+class autocontroller extends Controller
+{
+    public function ListarTodos(Request $request){
+        return auto::all();
+    }
+
+    public function ListarUno(Request $request, $idAuto){
+        return auto::findOrFail($idAuto);
+    }
+
+    public function EliminarUno(Request $request, $idAuto){
+        $auto = auto::findOrFail($idAuto);
+        $auto -> delete();
+
+        return ["mensaje" => "El auto con el id $idAuto ha sido eliminado";]
+    }
+
+    public function Insertar(Request $request){
+        $auto = new auto;
+        $auto -> marca = $request -> post ('marca');
+        $auto -> modelo = $request -> post ('modelo');
+        $auto -> color = $request -> post ('color');
+        $auto -> puertas = $request -> post ('puertas');
+        $auto -> cilindrado = $request -> post ('cilindrado');
+        $auto -> automatico = $request -> post ('automatico');
+        $auto -> electronico = $request -> post ('electronico');
+
+        $auto -> save();
+        return $auto;
+    }
+
+    public function Modificar(Request $request, $idAuto){
+        $auto = auto::findOrFail($idAuto);
+        
+        $auto -> marca = $request -> post ('marca');
+        $auto -> modelo = $request -> post ('modelo');
+        $auto -> color = $request -> post ('color');
+        $auto -> puertas = $request -> post ('puertas');
+        $auto -> cilindrado = $request -> post ('cilindrado');
+        $auto -> automatico = $request -> post ('automatico');
+        $auto -> electronico = $request -> post ('electronico');
+
+        $auto -> save();
+        return $auto;
+    }
+}
---
+++ database/factories/autoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class autoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'marca' => $this->faker->firstName(),
+            'modelo'=> $this->faker->lastName(),
+            'color' => $this->faker->safeColorName(),
+            'puertas' => $this->faker->randomDigit(),
+            'cilindrado' => $this->faker->randomDigit(),
+            'automatico' => $this->faker->numberBetween($min=0, $max=1),
+            'electronico' => $this->faker->numberBetween($min=0, $max=1)
+        ];
+    }
+}
